e1f70839f699c668bd5d1d3ed2bffc3cb920a355
Yurii Kryvorotko <krivorotkokrivorotko2016@gmail.com>
2024-11-04 23:44:36 +0200

    added container

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './styles/main.scss';
 import { Outlet } from 'react-router-dom';
+import styles from './styles/App.module.scss';
 
 export const App: React.FC = () => {
   return (
     <div className="container">
       <h1>TEAM 3!!! Welcome to Nice Gadgets store!</h1>
-      <Outlet />
+      <div className={styles.container}>
+        <Outlet />
+      </div>
     </div>
   );
 };
