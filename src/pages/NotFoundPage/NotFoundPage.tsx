b13f348405a9478ace33f7e5a4c4e87fc397054a
Yurii Kryvorotko <krivorotkokrivorotko2016@gmail.com>
2024-11-04 18:02:45 +0200

    notFoundPage imolemented

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import imageNotFound from '../../images/notFoundImage/page-not-found.png';
+import styles from './NotFoundPage.module.scss';
+import { Link } from 'react-router-dom';
 
 export const NotFoundPage: React.FC = () => (
-  <div>
-    <h1 className="title">Page not found</h1>
-  </div>
+  <>
+    <div className={styles.notFound}>
+      <img
+        src={imageNotFound}
+        alt="image of not found page"
+        className={styles.notFound__image}
+      />
+      <h2 className={styles.notFound__title}>Page not found</h2>
+      <Link to={'/'} className={styles.notFound__button}>
+        Back to home page
+      </Link>
+    </div>
+  </>
 );
