359e424cd2b943551df09efe5f68123568c7036e
Yurii Kryvorotko <krivorotkokrivorotko2016@gmail.com>
2024-11-05 19:20:09 +0200

    started homePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,3 +1,19 @@
 import React from 'react';
+import styles from './HomePage.module.scss';
+import { MainImageSlider } from './components/MainImageSlider/MainImageSlider';
 
-export const HomePage: React.FC = () => <h1 className="title">Home Page</h1>;
+export const HomePage: React.FC = () => {
+  return (
+    <div className={styles.homePage}>
+      <div className={styles.homePage__box}>
+        <h1 className={styles.homePage__title}>
+          Welcome to Nice Gadgets store!
+        </h1>
+      </div>
+
+      <div className={styles.homePage__swiperBox}>
+        <MainImageSlider />
+      </div>
+    </div>
+  );
+};
