925eac7b32cbf0c1c25f7c584f172812736a2311
Vitalii <mlineckiyvitaliy@gmail.com>
2024-11-13 12:50:33 +0200

    feat: minor changes

--- src/redux/slices/contactusSlice.ts
+++ src/redux/slices/contactusSlice.ts
@@ -13,7 +13,7 @@ const initialState: ContactUsState = {
   isModalOpen: false,
   mobileNumber: '',
   isRequestSent: false,
-  timer: 1000,
+  timer: 30000,
   showTimer: true,
 };
 
@@ -33,14 +33,14 @@ const contactUsSlice = createSlice({
     },
     sendRequest(state) {
       state.isRequestSent = true;
-      state.timer = 1000;
+      state.timer = 30000;
       state.showTimer = true;
     },
     resetRequest(state) {
       state.isModalOpen = false;
       state.mobileNumber = '';
       state.isRequestSent = false;
-      state.timer = 1000;
+      state.timer = 30000;
       state.showTimer = true;
     },
     setTimer(state, action: PayloadAction<number>) {
